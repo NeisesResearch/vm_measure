abff675bf17a7bfc464e2adae4e607cf44844fff
Michael Neises <neisesmichael@gmail.com>
2021-08-27 12:12:12 -0500

    prepare for more than one measurement

--- components/MeasurementManager/measurement_manager.c
+++ components/MeasurementManager/measurement_manager.c
@@ -25,34 +25,39 @@ int run(void)
     memset(msmt_data, '0', 4096);
     printf("VM Dataport reset!\n");
     //strcpy(msmt_data, "This is a crossvm dataport test string");
-
-    // wait until the module analyzer is ready
-    modules_analyzer_ready_wait();
-
-    // wait until the Linux kernel module is ready
-    msmt_module_ready_wait();
-    printf("Measurement Module Ready Signal Received\n");
-
+    
     // prepare for a full payload of measurements
     HashedModuleMeasurement** measurements = malloc(46 * sizeof(HashedModuleMeasurement*));
     int numMeasurements = 0;
 
+    // wait until the module analyzer is ready
+    modules_analyzer_ready_wait();
+    
+    // wait until the Linux kernel module is ready
+    msmt_module_ready_wait();
+    printf("Measurement Module Ready Signal Received\n");
+
+
     while(1)
     {
-        printf("Request Measurement\n");
-        msmt_component_done_emit_underlying();
-        msmt_module_ready_wait();
-
-        if(IsFinalPayload((uint8_t*)msmt_data))
+        // wait until I should request a measurement
+        // ...
+        printf("Request Module Measurements\n");
+        while(1)
         {
-            printf("Request Analysis\n");
-            RequestModulesMeasurement(msmt_data);
-            printf("Report: %s\n", (char*)modules_data);
-            break;
-        }
-        else
-        {
-            RequestModulesMeasurement(msmt_data);
+            msmt_component_done_emit_underlying();
+            msmt_module_ready_wait();
+            if(IsFinalPayload((uint8_t*)msmt_data))
+            {
+                printf("Request Analysis\n");
+                RequestModulesMeasurement(msmt_data);
+                printf("Report: %s\n", (char*)modules_data);
+                break;
+            }
+            else
+            {
+                RequestModulesMeasurement(msmt_data);
+            }
         }
         break;
     }
