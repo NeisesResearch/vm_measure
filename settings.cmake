a91832e18aead1c304d70042c9594d7285208903
Michael Neises <neisesmichael@gmail.com>
2022-03-07 14:19:56 -0600

    rebase from a vm_multi fork

--- settings.cmake
+++ settings.cmake
@@ -4,16 +4,21 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-set(supported "qemu-arm-virt")
+set(supported "exynos5422;qemu-arm-virt")
 if(NOT "${PLATFORM}" IN_LIST supported)
     message(FATAL_ERROR "PLATFORM: ${PLATFORM} not supported.
          Supported: ${supported}")
 endif()
-
-set(VmPCISupport ON CACHE BOOL "" FORCE)
 set(LibUSB OFF CACHE BOOL "" FORCE)
-
-# force cpu
-set(QEMU_MEMORY "2048")
-set(KernelArmCPU cortex-a53 CACHE STRING "" FORCE)
+set(VmPCISupport ON CACHE BOOL "" FORCE)
+set(VmVirtioConsole ON CACHE BOOL "" FORCE)
+set(VmVirtioNetArping OFF CACHE BOOL "" FORCE)
+set(VmVirtioNetVirtqueue ON CACHE BOOL "" FORCE)
 set(VmInitRdFile ON CACHE BOOL "" FORCE)
+if("${PLATFORM}" STREQUAL "qemu-arm-virt")
+    set(QEMU_MEMORY "2048")
+    set(KernelArmCPU cortex-a53 CACHE STRING "" FORCE)
+    set(qemu_sim_extra_args "-netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no -device virtio-net,netdev=mynet0,mac=52:55:00:d1:55:01")
+    set(KernelArmExportPCNTUser ON CACHE BOOL "" FORCE)
+    set(KernelArmExportPTMRUser ON CACHE BOOL "" FORCE)
+endif()
--- vm_measure_wiki/MeasurementTodo.md
+++
@@ -1,9 +0,0 @@
-- [x] Implement dataport data transfer
-- [x] Implement dataport signals
-- [x] Collect initial measurement data
-- [x] Chunk up initial measurement data
-- [x] Send initial measurement data to CAmkES
-- [x] Analyze initial measurement data in CAmkES
-- [x] Ensure measurement of poison module
-- [x] Implement sending rodata
-
